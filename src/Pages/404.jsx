923920e590ceaa85158b7ebe3e58140b62fee033
vindrayulian <vindrayulian@gmail.com>
2024-08-21 23:44:51 +0700

    feat : error notification

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
 
     return (
         <div className="flex justify-center items-center min-h-screen flex-col text-center gap-y-5">
-            <h1 className="text-5xl font-bold">Oops!</h1>
+            <h1 className="text-5xl font-bold">Oopss!</h1>
             <p className="my-5 text-xl">Sorry, an unexpected error has occurred.</p>
             <p className="text-lg">
                 {error.statusText || error.message}
